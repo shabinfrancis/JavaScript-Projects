4d321ef8bf158aa142e948d14da18d6d036b8235
Shabin Francis <shabinfrancis25@gmail.com>
2025-03-06 23:34:22 +0530

    First vommit

--- realtime-tracker/app.js
+++ realtime-tracker/app.js
@@ -12,8 +12,15 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", function(socket) {
+    socket.on("send-location", (data) => {
+        io.emit("receive-location", {id: socket.id, ...data})
+    })
     console.log("a user connected");
-})
+
+    socket.on("disconnect", () => {
+        io.emit("user-disconnected", socket.id);
+    })
+});
 
 app.get("/", (req, res) => {
     res.render("index");
--- realtime-tracker/public/js/script.js
+++ realtime-tracker/public/js/script.js
@@ -1,2 +1,48 @@
 const socket = io();
-console.log("Hey");
+/* Check if the browser supports geo location */
+if(navigator.geolocation) { 
+    /* Use watchPosition to track the users location continuously. */
+    navigator.geolocation.watchPosition(
+        /* Emit the latitude and longitude via a socket with "send-location". */
+        (position) => {
+            const {latitude, longitude} = position.coords;
+            socket.emit("send-location", {latitude, longitude});
+        },
+        /* Log any errors to the console. */
+        (error) => {
+            console.error(error);
+        }, 
+        {
+            /* Set options for high accuracy, a 5-second timeout and a no caching. */
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        }
+    )
+}
+/* Initialize a map centered at coordinates (0, 0) with a zoom level of 15 using leaflet. Add OpenStreetMap tiles to the map*/ 
+const map = L.map("map").setView([0, 0], 10);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "OpenStreetMap"
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+    const {id, latitude, longitude} = data;
+    map.setView([latitude, longitude], 16);
+    if(markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    }
+    else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+        
+    }
+});
+
+socket.on("user-disconnected", (id) => {
+    if(markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
